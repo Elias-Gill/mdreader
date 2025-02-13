7d6aeb37419409584d72d37d5075315ed08aac0f
Elias Gill <eliasgill42@gmail.com>
2025-02-13 11:33:33 -0300

    feat: headless mode

--- src/main.zig
+++ src/main.zig
@@ -13,29 +13,35 @@ const WebView = @import("webview").WebView;
 
 pub fn main() !void {
     var server_thread = try std.Thread.spawn(.{}, http.startHttpServer, .{});
-    // std.time.sleep(200 * std.time.ns_per_ms); // Espera 500ms antes de volver a intentar
 
     // parse command-line args
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
+    if (args.len <= 1) {
+        std.log.err("Debe proporcionar un archivo", .{});
+        return;
+    }
+
+    const fileName = args[1];
+
+    // Headless flag, don't instantiate the webview instance
+    if (std.mem.eql(u8, fileName, "--headless")) {
+        server_thread.join();
+        return;
+    }
+
     var w = WebView.create(false, null);
     try w.setTitle("Markdown Viewer");
-    try w.navigate("http://localhost:9090/app/home"); // home window, dont touch
 
-    // load file if provided
-    if (args.len > 1) {
-        const fileName = args[1];
+    // Allocate space for the URL + null terminator
+    const route = try std.fmt.allocPrintZ(allocator, "http://localhost:9090/{s}", .{fileName});
+    defer allocator.free(route);
 
-        // Allocate space for the URL + null terminator
-        const route = try std.fmt.allocPrintZ(allocator, "http://localhost:9090/{s}", .{fileName});
-        defer allocator.free(route);
+    std.debug.print("Navigating to: {s}\n", .{route});
 
-        std.debug.print("Navigating to: {s}\n", .{route});
-
-        // Use the null-terminated string safely
-        try w.navigate(route);
-    }
+    // Use the null-terminated string safely
+    try w.navigate(route);
 
     try w.run();
     w.destroy() catch {
